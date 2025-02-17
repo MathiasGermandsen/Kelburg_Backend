a0120ebd4caa4427978775dc53839114374d3967
mathias <mathias.germandsen@bestseller.com>
2025-02-17 12:01:11 +0100

    test50505

--- KelBurgAPI/Controllers/UsersController.cs
+++ KelBurgAPI/Controllers/UsersController.cs
@@ -66,6 +66,7 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet("read")]
+    [Authorize]
     public async Task<ActionResult<IEnumerable<Users>>> GetUsers(int? userId, string? FirstName, string? LastName, int pageSize = 100, int pageNumber = 1) 
     {
         if (pageNumber < 1 || pageSize < 1)
@@ -134,7 +135,7 @@ public class UsersController : ControllerBase
             new Claim(ClaimTypes.Name, user.Email)
         };
 
-        SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes
+        SymmetricSecurityKey key = new SymmetricSecurityKey(Convert.FromBase64String
             (_configuration["JwtSettings:Key"]));
         SigningCredentials? creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 

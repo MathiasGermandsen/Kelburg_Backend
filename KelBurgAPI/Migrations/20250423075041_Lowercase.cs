b3c5a03ed9a17f7a3c3e8d8a8d2d82bd70d4abb3
ArianYN <128600124+ArianYN@users.noreply.github.com>
2025-04-23 09:51:29 +0200

    Added Migration

---
+++ KelBurgAPI/Migrations/20250423075041_Lowercase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KelBurgAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Lowercase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
